41e34a322ca692d365387d3f7c063ba3cda91487
Ralph Trane <rmtrane@gmail.com>
2025-02-19 15:20:42 -0600

    Initial commit

---
+++ R/misc.R
@@ -0,0 +1,119 @@
+table_values <- function(
+    cur_col = "selsibtype",
+    file = getOption("wlsDataPath")) {
+  x <- haven::read_dta(file, col_select = dplyr::all_of(cur_col))[[1]]
+
+  value_labels <- attr(x, "labels")
+  x <- haven::zap_labels(x)
+
+  tbl <- table(x)
+
+  for_out <- dplyr::tibble(
+    values = if (is.character(x)) names(tbl) else as.numeric(names(tbl)),
+    n = as.numeric(tbl)
+  )
+
+  if (!is.null(value_labels)) {
+    for_out <- merge(
+      for_out,
+      dplyr::tibble(
+        values = unname(value_labels),
+        label = names(value_labels)
+      ),
+      by = "values",
+      all = T
+    )
+  }
+
+  for_out <- within(for_out, {
+    # within(for_out, {
+    n[is.na(n)] <- 0
+    valid <- values >= 0
+    percent <- n / sum(n)
+    percent_valid <- ifelse(values < 0, NA, n / sum(n[values >= 0]))
+  })
+
+  reactable::reactable(
+    for_out[, intersect(c("values", "label", "valid", "n", "percent", "percent_valid"), colnames(for_out))],
+    # outlined = T,
+    # highlight = T,
+    columns = list(
+      values = reactable::colDef(
+        name = "Value",
+        maxWidth = 12 * 8
+      ),
+      label = reactable::colDef(name = "Label"),
+      valid = reactable::colDef(
+        name = "Valid",
+        cell = function(value) {
+          if (value) {
+            shiny::tagAppendAttributes(
+              shiny::icon("circle-check", class = "fas"),
+              style = "color: green;"
+            )
+          } else {
+            shiny::tagAppendAttributes(
+              shiny::icon("circle-xmark", class = "fas"),
+              style = "color: red;"
+            )
+          }
+        },
+        align = "center",
+        vAlign = "top",
+        width = 12 * 6
+      ),
+      n = reactable::colDef(
+        name = "Frequency",
+        # align = "left",
+        width = 12 * nchar("Frequency"),
+        format = reactable::colFormat(separators = T)
+      ),
+      percent = reactable::colDef(
+        name = "Percent of all",
+        style = \(value) bar_style(width = value),
+        align = "left",
+        vAlign = "top",
+        format = reactable::colFormat(percent = T, digits = 1),
+        width = 12 * nchar("percent of all")
+      ),
+      percent_valid = reactable::colDef(
+        name = "Percent of valid",
+        style = \(value) bar_style(width = value),
+        align = "left",
+        vAlign = "top",
+        format = reactable::colFormat(percent = T, digits = 1),
+        width = 12 * nchar("percent of valid")
+      )
+    )[colnames(for_out)],
+    showPageSizeOptions = T,
+    defaultPageSize = 25,
+    pageSizeOptions = c(10, 25, 50)
+  )
+}
+
+
+bar_style <- function(
+    width = 1,
+    fill = "#D9D9D9",
+    height = "1.5em",
+    align = c("left", "right"),
+    backgroundPosition = "0.25em 0.375em",
+    color = "black") {
+  align <- match.arg(align)
+  # vAlign <- match.arg(vAlign)
+  if (align == "left") {
+    position <- paste0(width * 100, "%")
+    image <- sprintf("linear-gradient(90deg, %1$s %2$s, transparent %2$s)", fill, position)
+  } else {
+    position <- paste0(100 - width * 100, "%")
+    image <- sprintf("linear-gradient(90deg, transparent %1$s, %2$s %1$s)", position, fill)
+  }
+  list(
+    backgroundImage = image,
+    backgroundSize = paste("100%", height),
+    backgroundRepeat = "no-repeat",
+    # backgroundPosition = vAlign,
+    backgroundPosition = backgroundPosition,
+    color = color
+  )
+}
---
+++ R/wlsDataBrowser.R
@@ -0,0 +1,157 @@
+wlsDataBrowser <- function() {
+  old_shiny.maxRequestSize <- getOption("shiny.maxRequestSize")
+
+  options(shiny.maxRequestSize = 1100 * 1024^2)
+
+  wls_data_path_option <- "wlsDataPath0"
+
+  ui <- miniUI::miniPage(
+    theme = bslib::bs_theme(version = 4),
+    ## Change how selected rows are highlighted. (This "hack" works in conjunction with DT::selectRows which removes selections when not used.)
+    shiny::tags$style(
+      shiny::HTML("table.dataTable tr.active td, table.dataTable td.active {box-shadow: inset 0 0 0 9999px #e7e7e7 !important;}"),
+      shiny::HTML("td {color: black !important;}")
+    ),
+    miniUI::gadgetTitleBar("WLS Data Browser"),
+    miniUI::miniContentPanel(
+      if (is.null(getOption(wls_data_path_option))) {
+        shiny::fileInput(
+          "wls_data_path",
+          label = "Choose input file",
+          accept = "dta"
+        )
+      },
+      shiny::checkboxInput(
+        "value_tables",
+        "Show tables of values in data when rows are clicked (warning: slow!)",
+        width = "100%"
+      ),
+      DT::dataTableOutput("wlsData")
+    )
+  )
+
+  server <- function(input, output, session) {
+    wls_data_path <- shiny::reactiveVal(getOption(wls_data_path_option))
+    wls_data_tabl <- shiny::reactiveVal(getOption(wls_data_path_option))
+
+    shiny::observe(
+      wls_data_path(input$wls_data_path$datapath)
+    ) |>
+      shiny::bindEvent(input$wls_data_path)
+
+    shiny::observe({
+      if (!is.null(wls_data_path())) {
+        wls_data <- haven::read_dta(file = wls_data_path(), n_max = 0)
+
+        out <- data.frame(
+          var_name = colnames(wls_data),
+          labels = unname(unlist(lapply(wls_data, \(x) attr(x, "label")))),
+          stringsAsFactors = F
+        )
+
+        out$visit <- factor(
+          unlist(lapply(strsplit(out$labels, " - "), `[[`, 1)),
+          levels = c("SD", "AD", paste0("R0", 1:8))
+        )
+
+        out$labels <- gsub(
+          pattern = paste(levels(out$visit), collapse = " - |"),
+          replacement = "",
+          x = out$labels
+        )
+
+        wls_data_tabl(out)
+      }
+    })
+
+    output$wlsData <- DT::renderDataTable({
+      if (!is.null(wls_data_tabl())) {
+        DT::datatable(
+          wls_data_tabl()[, c("var_name", "visit", "labels")],
+          colnames = c(
+            "Variable name (as in data file)" = "var_name",
+            "Round" = "visit",
+            "Variable Label (from data file)" = "labels"
+          ),
+          rownames = F,
+          filter = "top",
+          class = "row-border compact hover",
+          options = list(
+            pageLength = 20,
+            lengthMenu = c(10, 20, 50, 100, 200)
+          ),
+          selection = "single" # if (input$value_tables) "single" else "none"
+        )
+      }
+    })
+
+    wlsDataProxy <- DT::dataTableProxy("wlsData")
+
+    freq_table <- shiny::reactiveVal()
+
+    shiny::observe({
+      if (!is.null(input$wlsData_rows_selected)) {
+        if (input$value_tables) {
+          shinycssloaders::showPageSpinner()
+          cur_col <- wls_data_tabl()$var_name[input$wlsData_rows_selected]
+
+          freq_table(table_values(cur_col))
+          shinycssloaders::hidePageSpinner()
+        }
+      }
+    }) |>
+      shiny::bindEvent(input$wlsData_rows_selected)
+
+    output$freq_table <- reactable::renderReactable({
+      if (inherits(freq_table(), "reactable")) {
+        freq_table()
+      }
+    })
+
+    output$for_modal <- shiny::renderUI({
+      if (inherits(freq_table(), "reactable")) {
+        reactable::reactableOutput("freq_table")
+      } else {
+        shiny::h2(freq_table())
+      }
+    })
+
+    shiny::observe({
+      if (input$value_tables) {
+        shiny::showModal(
+          shiny::modalDialog(
+            shiny::tagList(
+              shiny::uiOutput("for_modal")
+            ),
+            title = shiny::HTML(with(wls_data_tabl()[input$wlsData_rows_selected, ], paste0(var_name, ": ", labels))),
+            size = "xl",
+            footer = shiny::actionButton("return", "Return")
+          )
+        )
+      } else {
+        DT::selectRows(proxy = wlsDataProxy, selected = NULL)
+      }
+    }) |>
+      shiny::bindEvent(input$wlsData_rows_selected)
+
+    shiny::observe({
+      DT::selectRows(proxy = wlsDataProxy, selected = NULL)
+      shiny::removeModal()
+    }) |>
+      shiny::bindEvent(input$return)
+
+    shiny::observe({
+      shiny::stopApp()
+      options(old_shiny.maxRequestSize = NULL)
+    }) |>
+      shiny::bindEvent(input$done)
+
+    session$onSessionEnded(function() {
+      shiny::stopApp()
+      options(old_shiny.maxRequestSize = NULL)
+    })
+  }
+
+  viewer <- shiny::browserViewer()
+  shiny::runGadget(ui, server, viewer = viewer)
+}
